0d1dbc1e6b378ad9bfd06cf73608d27ee901f70f
Michel Selerin <michel.selerin@outlook.com>
2018-10-03 14:35:17 +0200

    test: fix unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,11 +18,6 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'ngx-security-app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ngx-security-app');
-  }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
