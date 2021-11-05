b05b183ce76d8f7c60ecd8499dde9871df3968e9
Michel Selerin <Michel.Selerin@nsi-sa.be>
2021-11-05 15:37:11 +0100

    chore: merge from master

--- projects/demo/src/app/security.service.ts
+++ projects/demo/src/app/security.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import {BehaviorSubject, Observable, of} from "rxjs";
 import {map} from "rxjs/operators";
 
+const AUTH_GROUPS = ['GROUP_A', 'GROUP_B'];
+const AUTH_ROLES = ['ADMIN', 'USER'];
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +13,27 @@ export class SecurityService {
 
   public isAuthenticated(): Observable<boolean> {
     return this.authenticated$;
+  }
+
+  public switchAuthentication(): void {
+    const authenticated = !this.authenticated$.value;
+    this.authenticated$.next(authenticated);
+  }
+
+  public isMemberOf(group: string): Observable<boolean> {
+    if (this.authenticated$.value) {
+      return of(AUTH_GROUPS.includes(group));
+    }
+
+    return of(false);
+  }
+
+  public hasRole(role: string): Observable<boolean> {
+    if (this.authenticated$.value) {
+      return of(AUTH_ROLES.includes(role));
+    }
+
+    return of(false);
   }
 
   public hasPermission(perm: string, resource?: any): Observable<boolean> {
